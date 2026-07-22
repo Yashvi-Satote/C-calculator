5ecb1a90f9c0888c1f7f882223d290a29975d30b
yashvi-satote <yashvisatote4@gmail.com>
2026-07-22 20:10:42 +0530

    new features

--- main.c
+++ main.c
@@ -15,7 +15,7 @@ void Add(){
     printf("\n Enter no. 2: ");
     scanf("%d", &b);
 
-    printf("Sum of both numbers is: %d", a+b);
+    printf("\nSum of both numbers is: %d", a+b);
 
     printf("\n\nPress enter...");
     getch();
@@ -33,7 +33,7 @@ void Subtract(){
     printf("\n Enter no. 2: ");
     scanf("%d", &b);
 
-    printf("Subtraction of both numbers is: %d", a-b);
+    printf("\nSubtraction of both numbers is: %d", a-b);
 
     printf("\n\nPress enter...");
     getch();
@@ -50,7 +50,7 @@ void Multiply(){
     printf("\n Enter no. 2: ");
     scanf("%d", &b);
 
-    printf("Product of both numbers is: %d", a*b);
+    printf("\nProduct of both numbers is: %d", a*b);
 
     printf("\n\nPress enter...");
     getch();
@@ -70,11 +70,164 @@ void Devide(){
         printf("Cannot devide by zero.");
         return;
     }
-    printf("Sum of both numbers is: %d", a/b);
+    printf("\nSum of both numbers is: %d", a/b);
 
     printf("\n\nPress enter...");
     getch();
 }
+
+void reverse(){
+    printf("\n---REVERSE NUMBER---\n");
+
+    int a=0, res=0;
+    printf("\n Enter number: ");
+    scanf("%d", &a);
+
+    while (a!=0)
+    {
+        int r= a%10;
+        res= res*10 + r;
+        a=a/10;
+    }
+    
+
+    printf("\nReversed Number is: %d", res);
+
+    printf("\n\nPress enter...");
+    getch();
+
+
+}
+
+void countDig(){
+    printf("\n---COUNT DIGITS OF A NUMBER---\n");
+
+    int a=0, sum=0;
+    printf("\n Enter number: ");
+    scanf("%d", &a);
+
+    while (a!=0)
+    {
+        sum++;
+        a=a/10;
+    }
+    
+
+    printf("\nCount of digits is: %d", sum);
+
+    printf("\n\nPress enter...");
+    getch();
+    
+}
+
+void sumDig(){
+    printf("\n---SUM OF ALL DIGITS---\n");
+
+    int a=0, sum=0;
+    printf("\n Enter number: ");
+    scanf("%d", &a);
+
+    while (a!=0)
+    {
+        int r= a%10;
+        sum+=r;
+        a=a/10;
+    }
+    
+
+    printf("\nSum of all digits is: %d", sum);
+
+    printf("\n\nPress enter...");
+    getch();
+    
+}
+
+void gcd(){
+    
+}
+
+void lcm(){
+    
+}
+
+void mulTable(){
+    printf("\n---MULTIPLICATION TABLE---\n");
+
+    int a=0;
+    printf("\n Enter number: ");
+    scanf("%d", &a);
+
+        for(int i=1; i<=10; i++){
+            int b= a*i;
+            printf("%d x %d = %d\n", a, i, b);
+        }
+
+    printf("\n\nPress enter...");
+    getch();
+}
+
+void sumtillN(){
+
+    printf("\n---SUM OF ALL NUMBERS TILL N---\n");
+
+    int n=0,sum=0;
+    printf("\n Enter n: ");
+    scanf("%d", &n);
+
+        for(int i=0; i<=n; i++){
+            sum= sum + i;
+        }
+
+        printf("\nTotal sum is: %d", sum);
+
+    printf("\n\nPress enter...");
+    getch();
+    
+}
+
+void oddtillN(){
+    
+    printf("\n---SUM OF ALL ODD NO. TILL N---\n");
+
+    int n=0,sum=0;
+    printf("\n Enter n: ");
+    scanf("%d", &n);
+
+        for(int i=0; i<=n; i++){
+            if(i%2!=0){
+                sum= sum + i;
+            }
+            
+        }
+
+        printf("\nSum of all Odd numbers is: %d", sum);
+
+    printf("\n\nPress enter...");
+    getch();
+}
+
+void eventillN(){
+
+    printf("\n---SUM OF ALL EVEN NO. TILL N---\n");
+
+    int n=0,sum=0;
+    printf("\n Enter n: ");
+    scanf("%d", &n);
+
+        for(int i=0; i<=n; i++){
+            if(i%2==0){
+                sum= sum + i;
+            }
+            
+        }
+
+        printf("\nSum of all Even numbers is: %d", sum);
+
+    printf("\n\nPress enter...");
+    getch();
+    
+}
+    
 
 int main(){
     int choice= -1;
@@ -86,15 +239,20 @@ int main(){
         printf("\n 2: Subtaction of two numbers."); //done
         printf("\n 3: Multiplication of two numbers.");  //done
         printf("\n 4: Devision of two numbers."); //done
-        printf("\n 5: Reverse a number.");
-        printf("\n 6: Count Digits.");
-        printf("\n 7: Sum of digits.");
+        printf("\n 5: Reverse a number."); //done
+        printf("\n 6: Count Digits."); //done
+        printf("\n 7: Sum of digits."); //done
         printf("\n 8: Greatest common devisor (GCD)");
         printf("\n 9: Lowest common multiple (LCM)");
-        printf("\n 10: Multiplication Table");
-        printf("\n 11: Sum of first N natural numbers");
-        printf("\n 12: Sum of all odd no. till N");
-        printf("\n 12: Sum of all even no. till N");
+        printf("\n 10: Multiplication Table"); //done
+        printf("\n 11: Sum of first N natural numbers"); //done
+        printf("\n 12: Sum of all odd no. till N"); //done
+        printf("\n 13: Sum of all even no. till N"); //done
+        printf("\n 14: List of all factors of a number");
+        printf("\n 15: nth Fibonacci number ");
+        printf("\n 16: Electricity Bill Calculator");
+        printf("\n 17: Square Root");
+        printf("\n 18: Power of n");
         printf("\n 0: Exit program");
 
         printf("\n\n Enter choice: ");
@@ -129,6 +287,73 @@ int main(){
             system("cls");
             break;
 
+            case 5:
+
+            system("cls");
+            reverse();
+            system("cls");
+            break;
+
+            case 6:
+
+            system("cls");
+            countDig();
+            system("cls");
+            break;
+
+            case 7:
+
+            system("cls");
+            sumDig();
+            system("cls");
+            break;
+
+            case 8:
+
+            system("cls");
+            gcd();
+            system("cls");
+            break;
+
+            case 9:
+
+            system("cls");
+            lcm();
+            system("cls");
+            break;
+
+            case 10:
+
+            system("cls");
+            mulTable();
+            system("cls");
+            break;
+
+            case 11:
+
+            system("cls");
+            sumtillN();
+            system("cls");
+            break;
+
+            case 12:
+
+            system("cls");
+            oddtillN();
+            system("cls");
+            break;
+
+            case 13:
+
+            system("cls");
+            eventillN();
+            system("cls");
+            break;
+
+            
+            
+            
+            
             case 0:
             //program ended
             system("cls");
